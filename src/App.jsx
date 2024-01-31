4fad1fc82ba1b50c4dfcae9644fbd43c37d6add3
mohanavamsi0614 <mohana.gangisetti@kalvium.community>
2024-01-31 10:21:43 +0530

    added

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState,useRef } from 'react'
+import './App.css'
+import data from './data'
+function App() {
+  const [state, setstate] = useState("")
+  let refe=useRef()
+  document.body.onkeydown=(e)=>{
+    if(e.key=="Escape"){
+    refe.current.style.display="none"
+    console.log("Escape")
+    }}
+  return (
+    <div>
+      <input onChange={(e)=>{setstate(e.target.value)
+      refe.current.style.display="block"
+      }}
+       />
+      <div ref={refe}>
+        {data.filter((i)=>{return state!==""?i.name.startsWith(state):0}).map((i,ind)=>
+        <h1 key={ind} >{i.name}</h1>)
+        }
+      </div>
+    </div>
+  )
+}
+
+export default App
